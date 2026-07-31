2b25c3ed4bc724d056f0df605d7178fd1408712b
Mary Harmon <mary.harmon@cgi.com>
2026-07-31 12:42:01 +0100

    Session defaults set and if statements added for routes

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,30 +1,30 @@
 module.exports = {
 
   // Insert values here
-
+  "journey": "profile",
   // Defaults page view to native
   "web": "no", 
 
   // Set linked profile details
-  "linkedProfileFirstName": "Toby",
-  "linkedProfileLastName": "Day",
-  "linkedProfileAge": "9",
+  "linkedProfileFirstName": "Clive",
+  "linkedProfileLastName": "Campbell",
+  "linkedProfileAge": "43",
   "linkedProfileNhsNumber": "321 654 0987",
-  "linkedProfileDob": "5 November 2015",
+  "linkedProfileDob": "5 November 1982",
 
   // Set user details
-  "userFirstName": "Neil",
-  "userLastName": "Dean",
-  "nhsNumber": "123 456 7890",
-  "dob": "15 March 1984",
+  "userFirstName": "Josiah",
+  "userLastName": "Campbell",
+  "nhsNumber": "345 598 673 219",
+  "dob": "17 December 1942",
 
   // Set version of the app
   "version": "2.36.6 (2.36.0)",
 
   // Messages
-  "message1read": "false", 
-  "message2read": "false", 
-  "messages": 2, // This displays in the messages badge - set this to the number of unread messages - For example, if 'message1read = true' and 'message2read = false', then set the 'messages' number to 1
+  "message1read": "true", 
+  "message2read": "true", 
+  "messages": 0, // This displays in the messages badge - set this to the number of unread messages - For example, if 'message1read = true' and 'message2read = false', then set the 'messages' number to 1
   "messageSendingError": "true", // Shows 'cannot send your reply' screen for message 2
 
   // Set account type - p9, p5 or linked-profiles
